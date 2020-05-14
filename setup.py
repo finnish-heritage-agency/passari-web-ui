7968edc448b11e3ad6abceffc7feaed8950d09ba
Janne Pulkkinen <janne.pulkkinen@museovirasto.fi>
2020-05-14 12:03:10 +0300

    Small packaging changes

--- setup.py
+++ setup.py
@@ -19,10 +19,28 @@ setup(
     include_package_data=True,
     package_dir={"passari_web_ui": "src/passari_web_ui"},
     install_requires=[
-        "Flask", "Flask-Security-Too", "click", "SQLAlchemy", "psycopg2",
-        "rq", "rq-dashboard", "toml", "bcrypt", "Flask-SQLAlchemy",
-        "Flask-WTF", "flask-talisman", "arrow"
+        "Flask",
+        "Flask-Security-Too",
+        "click>=7", "click<8",
+        "SQLAlchemy",
+        "psycopg2",
+        "rq>=1",
+        "rq-dashboard>=0.6",
+        "toml",
+        "bcrypt",
+        "Flask-SQLAlchemy",
+        "Flask-WTF",
+        "flask-talisman",
+        "arrow"
     ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Flask"
+    ],
+    python_requires=">=3.6",
     use_scm_version=True,
     setup_requires=["setuptools_scm"]
 )
